88b91c99946f8cc2a8ff6fe18bc5e824f180bed7
Unknownday <unknownday.game@gmail.com>
2024-03-09 15:54:23 +0300

    Revert "Revert "Creating token-auth""
    This reverts commit 4243c61f0b393669b5ce01a9e1bbdbb86280c068.

--- TaskManager.API/Controllers/UserController.cs
+++ TaskManager.API/Controllers/UserController.cs
@@ -4,9 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Npgsql;
 using TaskManager.Common.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace TaskManager.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
@@ -117,7 +119,7 @@ namespace TaskManager.API.Controllers
                         command.Parameters.AddWithValue("@LastName", userModel.LastName);
                         command.Parameters.AddWithValue("@Password", userModel.Password);
                         command.Parameters.AddWithValue("@Phone", userModel.Phone);
-                        command.Parameters.AddWithValue("@Email", userModel.Email);
+                        command.Parameters.AddWithValue("@Email", GetUserEmailByToken(Request));
                         command.Parameters.AddWithValue("@Status", (int)userModel.Status);
                         await command.ExecuteNonQueryAsync();
                     }
@@ -270,6 +272,29 @@ namespace TaskManager.API.Controllers
                 // Логирование исключения
                 Console.WriteLine(ex.ToString());
                 return BadRequest();
+            }
+        }
+
+        private async Task<string> GetUserEmailByToken(HttpRequest request)
+        {
+            string token = request.Headers["Authorization"].ToString();
+            using (var connection = GetOpenConnection())
+            {
+                var sql = "SELECT * FROM tokens WHERE userToken=@Token";
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Token", token.Replace("Bearer ", ""));
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        string? email = null;
+                        while (reader.Read())
+                        {
+                            email = reader["Email"].ToString();
+                        }
+                        return email;
+                    }
+                    
+                }
             }
         }
     }
